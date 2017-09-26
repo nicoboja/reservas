71b9110c883080281818cccb7a2269268c4c249b
nicoboja <nico.boja@gmail.com>
2017-09-26 18:59:12 -0300

    .

--- src/data/FactoryConexion.java
+++ src/data/FactoryConexion.java
@@ -10,8 +10,8 @@ public class FactoryConexion {
 	private String host="localhost";
 	private String hostM="127.0.0.1";
 	private String port="3306";
-	private String user="root";
-	private String password="root";
+	private String user="java";
+	private String password="java";
 	private String db="reserva";
 	
 	private static FactoryConexion instancia;
@@ -46,6 +46,7 @@ public class FactoryConexion {
 				if(conn==null || conn.isClosed()){	
 					conn = DriverManager.getConnection(
 				        "jdbc:mysql://"+hostM+":"+port+"/"+db+"?user="+user+"&password="+password);
+					
 				}
 			}catch (SQLException ex){
 			throw new AppDataException(ex, "Error al conectar a la base de datos");
